8802e473328f4465103ffb25d7c6ad887c4d1e81
Silverplata <jplata@utedevas.es>
2025-08-03 01:21:28 +0200

    Fixed

--- generate_hash.py
+++ generate_hash.py
@@ -1,3 +1,3 @@
 from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-print(pwd_context.hash("password123"))
+print(pwd_context.hash("xxxxxxxxx"))
